0c7798133ccc7bd50e9b27acd9de9b6fbfc8b487
Luke Gerhart <luke_gerhart@hotmail.com>
2018-02-27 14:57:49 -0500

    q1 part b

--- hw2_homefield.py
+++ hw2_homefield.py
@@ -1,86 +1,96 @@
 import csv
-games = []
 
 def find_homefield(margins, iters, initial):
 	alpha = 0
 	for counter in range(1, 10000):
-		i = counter % len(margin_list)
+		i = counter % len(margins)
 		alpha = 2/counter
-		marg = margin_list[i]
+		marg = margins[i]
 		diff = marg - initial
 		diff = alpha*diff
 		initial = initial + diff
 	return initial
+	
+def margins(game_data, h, a):
+	margin_list = []
+	for game in game_data:
+		margin = int(game[h]) - int(game[a])
+		margin_list.append(margin)
+	return margin_list
 
-with open('nfl2016.csv', newline='') as csvfile:
-	reader = csv.reader(csvfile, delimiter=',')
-	for row in reader:
-		if row[0] == '' or row[0] == 'Winner/tie':
-			continue
-		games.append(row)
-margins = {}
-for game in games:
-	margin = 0
-	home = ''
-	if game[1] == '@':
-		home = game[2]
-		margin = int(game[4]) - int(game[3])
-	else:
-		home = game[0]
-		margin = int(game[3]) - int(game[4])
-	#print(margin)
-	if home in margins:
-		margins[home].append(margin)
-	else:
-		margins[home] = []
-		margins[home].append(margin)
+def nfl():
+	def get_margins(games):
+		margin_list = []
+		for game in games:
+			margin = 0
+			if game[1] == '@':
+				margin = int(game[4]) - int(game[3])
+			else:
+				margin = int(game[3]) - int(game[4])
+			margin_list.append(margin)
+		return margin_list
+	
+	def get_data(filename):
+		nflgames = []
+		with open(filename, newline='') as csvfile:
+			reader = csv.reader(csvfile, delimiter=',')
+			for row in reader:
+				if row[0] == '' or row[0] == 'Winner/tie':
+					continue
+				nflgames.append(row)
+		return nflgames
+	
+	print('NFL 2006 homefield advantage:', find_homefield(get_margins(get_data('nfl2006.csv')), 10000, 4))
+	print('NFL 2016 homefield advantage:', find_homefield(get_margins(get_data('nfl2016.csv')), 10000, 4))
 
-marginl = [x for x in margins.values()]
-margin_list = [item for sublist in marginl for item in sublist]
+def nhl():
+	
+	def get_data(filename):
+		nhlgames = []
+		with open(filename, newline='') as csvfile:
+			reader = csv.reader(csvfile, delimiter=',')
+			for row in reader:
+				modrow = row[0].split(',')
+				if modrow[0] == 'Date':
+					continue
+				nhlgames.append(modrow)
+		return nhlgames
+	
+	print('NHL 2006 homefield advantage:', find_homefield(margins(get_data('nhl2006.csv'), 4, 2), 10000, 4))
+	print('NHL 2016 homefield advantage:', find_homefield(margins(get_data('nhl2016.csv'), 4, 2), 10000, 4))
 
-print('NFL 2016 homefield advantage:', find_homefield(margin_list, 10000, 4))
+def nba():
+	
+	def get_data(filename):
+		nbagames = []
+		with open(filename, newline='') as csvfile:
+			reader = csv.reader(csvfile, delimiter=',')
+			for row in reader:
+				if len(row) == 0:
+					continue
+				nbagames.append(row)
+		return nbagames
+	
+	print('NBA 2006 homefield advantage:', find_homefield(margins(get_data('nba2006.csv'), 1, 0), 10000, 4))
+	print('NBA 2016 homefield advantage:', find_homefield(margins(get_data('nba2016.csv'), 1, 0), 10000, 4))
 
-games = []
-with open('nhl2016.csv', newline='') as csvfile:
-	reader = csv.reader(csvfile, delimiter=',')
-	for row in reader:
-		modrow = row[0].split(',')
-		if modrow[0] == 'Date':
-			continue
-		games.append(modrow)
+def mlb():
+	def get_data(filename):
+		mlbgames = []
+		with open(filename, newline='') as csvfile:
+			reader = csv.reader(csvfile, delimiter=',')
+			for row in reader:
+				if len(row) == 0:
+					continue
+				mlbgames.append(row)
+		for game in mlbgames:
+			game[1] = game[1].split('(')[1].split(')')[0]
+			game[0] = game[0].split('(')[1].split(')')[0]
+		return mlbgames
 
-margin_list = []
-for game in games:
-	margin = int(game[4]) - int(game[2])
-	margin_list.append(margin)
-print('NHL 2016 homefield advantage:', find_homefield(margin_list, 10000, 4))
-
-games = []
-with open('nba2016.csv', newline='') as csvfile:
-	reader = csv.reader(csvfile, delimiter=',')
-	for row in reader:
-		if len(row) == 0:
-			continue
-		modrow = row[0].split(',')
-		if modrow[0] == 'Date':
-			continue
-		games.append(modrow)
-
-margin_list = []
-for game in games:
-	margin = int(game[5]) - int(game[3])
-	margin_list.append(margin)
-print('NBA 2016 homefield advantage:', find_homefield(margin_list, 10000, 4))
-
-games = []
-with open('mlb2016.csv', newline='') as csvfile:
-	reader = csv.reader(csvfile, delimiter=',')
-	for row in reader:
-		games.append(row)
-
-margin_list = []
-for game in games:
-	margin = int(game[1].split('(')[1].split(')')[0]) - int(game[0].split('(')[1].split(')')[0])
-	margin_list.append(margin)
-
-print('MLB 2016 homefield advantage:', find_homefield(margin_list, 10000, 4))
+	print('MLB 2006 homefield advantage:', find_homefield(margins(get_data('mlb2006.csv'), 1, 0), 10000, 4))
+	print('MLB 2016 homefield advantage:', find_homefield(margins(get_data('mlb2016.csv'), 1, 0), 10000, 4))
+nfl()
+nhl()
+nba()
+mlb()
